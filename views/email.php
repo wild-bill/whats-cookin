b9a4a16ff8d5af6ebca1b8a1bc3a44d3a9905688
wild-bill <wild-bill@users.noreply.github.com>
2017-01-29 16:08:38 -0500

    added more customization to email

--- views/email.php
+++ views/email.php
@@ -1,15 +1,49 @@
 <?php
+
+//$to = 'doctor.gill.bates@gmail.com, mollylaflesh@gmail.com, william.kryjak@gmail.com';
+//$to = 'doctor.gill.bates@gmail.com, william.kryjak@gmail.com'; // note the comma
+$to = 'doctor.gill.bates@gmail.com';
+// Subject
+$subject = 'Meal Ingredients for the week';
+
+// Message
+$messageStart = "
+<html>
+<head>
+  <title>Where will this display?</title>
+</head>
+<body>
+<h4>Ingredients</h4>";
+
+$messageEnd = "</body>
+</html>";
+ob_start();
 //display the ingredients
-echo "<h4>Ingredients</h4>";
+//echo "<h4>Ingredients</h4>";
 	foreach ($ingredientPost as $value){
 		foreach($value as $lineitem){
 			echo "$lineitem, ";
 		}
 	}
 	foreach ($choices as $value){
-		echo "$value<br />";
+		echo "<br /><a href=\"http://localhost:8080/recipes/index.php?controller=recipes&action=show&recipe_id=".$value."\">".$names[$value]."Recipe Name</a>"; //the recipe name has to be taken diff. this takes the id'th place in the array which is not correct
 	}
 
+$displayed = ob_get_clean();
 
+$fullMessage = $messageStart . $displayed . $messageEnd;
+
+echo $fullMessage;
+$headers[] = 'MIME-Version: 1.0';
+$headers[] = 'Content-type: text/html; charset=iso-8859-1';
+
+// Additional headers
+//$headers[] = 'To: Mary <mary@example.com>, Kelly <kelly@example.com>';
+$headers[] = 'From: Wha\'ts Cookin\'?" <birthday@example.com>';
+//$headers[] = 'Cc: birthdayarchive@example.com';
+//$headers[] = 'Bcc: birthdaycheck@example.com';
+
+// Mail it
+mail($to, $subject, $fullMessage, implode("\r\n", $headers));
 
 ?>
