d1c53a4f53de51c4ab8414d31d91d1ab7b47256b
wild-bill <wild-bill@users.noreply.github.com>
2017-02-04 15:53:38 -0500

    Supports ingredient unit and amount
    Now will display the ingredient unit and amount instead of just the name. The style (not css) is a little different, as instead of all being in the box one after another, they are a list like line items

--- views/posts/show.php
+++ views/posts/show.php
@@ -1,5 +1,16 @@
-<p><h3><?php echo $post->recipe_name; //case sensitive?></h3></p>
-<p><?php echo $post->recipe_description; ?></p>
+<!--Slide Show -->
+<!--<section>
+	<img class="mySlides" src="views/meal1.jpg" style="width:100%">
+	<img class="mySlides" src="views/meal2.png" style="width:100%">
+	<img class="mySlides" src="views/meal3.jpeg" style="width:100%">
+</section>	-->
+
+
+<!-- Let the recipe nonsense begin...take out some of this code eventually it looks terrible!-->
+<div class="name"><p>
+<?php echo $post->recipe_name; //case sensitive?></p></div>
+<div class="description"><p>Feeds:  scale factor	| Takes:(recipe times, summed)  </p></div>
+<div class="description"><p><?php echo $post->recipe_description; ?></p></div>
 <p><?php //echo $secondPost->recipe_description; ?></p>
 <p><?php //echo $post->step_no; ?>
 <p><?php //echo $post->step_instructions; ?>
@@ -11,49 +22,28 @@
 <?php  //print_r($stepPost); echo("<br />");echo(count($stepPost)); echo("<br />");print_r(array_keys($stepPost)); //this line prints stuff for debugging mainly
 //this is for printing the entire steps array at once?>
 <?php 
-	
+	echo "<div class=\"ingredients\">";
 	echo "<h4>Ingredients</h4>";
 	foreach ($ingredientPost as $value){
-		echo "$value, ";
+		foreach ($value as $subvalue){
+			echo $subvalue." "; // here this is not an object. I don't know why. In email, it is an object and must be treated like an object
+		}
+		echo "<br />";
 	}
+	echo "</div>";
 	echo "<br />\n";
-	echo "<h4>Steps</h4>";
+	echo "<h4>Steps</h4><ul id = \"myUL\">";
 	$i=0;
 	foreach ($stepPost as $value){
 		if($i++ %2 == 0){
 			//if the index is an even number, it's a step no
-			echo "Step No. $value<br />\n";
+			echo "<li>Step No. $value<br />\n";
 		}
 			//if the index is an odd number, it's a step description
 		else{
-			echo "$value<br />\n";
+			echo "$value</li>\n";
 		}
 		
 	}
-	
-	
-	?>
-
-<form action="" method="post">
-    <input type="submit" value="Send details to embassy" />
-    <input type="hidden" name="button_pressed" value="1" />
-</form>
-
-<?php
-
-if(isset($_POST['button_pressed']))
-{
-    $to      = 'doctor.gill.bates@gmail.com';
-    $subject = 'the subject';
-    $message = 'hi sweetie pie';
-    $headers = 'From: doctor.gill.bates@gmail.com' . "\r\n" .
-        'Reply-To: doctor.gill.bates@gmail.com' . "\r\n" .
-        'X-Mailer: PHP/' . phpversion();
-
-    mail($to, $subject, $message, $headers);
-
-    echo 'Email Sent.';
-}
-
+	echo "</ul>";
 ?>
-
