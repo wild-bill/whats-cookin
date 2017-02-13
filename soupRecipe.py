9a8739093059df7428e0ec3ce546e91b281b57aa
wild-bill <wild-bill@users.noreply.github.com>
2017-02-13 16:03:27 -0500

    More efficient version
    This version is more efficient and the code is cleaned up. I'm not sure how practical it will be to split strings and try to make tables from there so I may just do manually.

--- soupRecipe.py
+++ soupRecipe.py
@@ -1,82 +1,83 @@
+#we want to create the soup only once for each link.
+#we want to open and write to the file only once per table
 from bs4 import BeautifulSoup
 import urllib.request
 import json
 import csv
 
-# 2.10.17
-"""This program will scrape recipe data from Foodnetwork.com. It would probably work on any other recipe sites that use JSON to store the data"""
+def createSoup(recipe):
+	r = urllib.request.urlopen(recipe).read()
+	soup = BeautifulSoup(r, 'html.parser')
+	
+	#find the JSON section in the HTML
+	json_tag = soup.find("script", type="application/ld+json") #that last part could be a variable I guess, based on recipe site
+	jString = json_tag.string
+	jData = json.loads(jString)
+	return jData
 
-# this should be a list of links so that the program can just loop through it and get a bunch of recipes
+def createTables(data):
+	for table in tables:
+		with open(table+'.csv', 'w', newline='\n') as f:
+			writer = csv.writer(f)
+			if table == 'recipe':
+				build_recipe_table(idCounter, writer)
+			elif table == 'step':
+				build_step_table(idCounter, writer)
+			elif table == 'step_ingredient':
+				build_step_ingredient_table(idCounter, writer)
+			elif table == 'unit':
+				build_unit_table(idCounter, writer)
+			elif table == 'ingredient':
+				build_ingredient_table(idCounter, writer)
+			else:
+				print("Something went wrong")
 
-
-
-
-
-recipes = ["http://www.foodnetwork.com/recipes/food-network-kitchen/eggplant-and-tofu-curry-recipe","http://www.foodnetwork.com/recipes/rachael-ray/spinach-and-artichoke-baked-whole-grain-pasta-recipe", "http://www.foodnetwork.com/recipes/easy-mushroom-vegetable-paella-recipe", "http://www.foodnetwork.com/recipes/rachael-ray/ravioli-vegetable-lasagna-recipe","http://www.foodnetwork.com/recipes/rachael-ray/instant-pesto-torta-with-bread-and-vegetables-recipe"]
-
-
-
-
-with open('recipes.csv', 'w', newline='\n') as f:
-	writer = csv.writer(f)
-	idCounter = 1
-#create the soup
-	for recipe in recipes:
-		r = urllib.request.urlopen(recipe).read()
-		soup = BeautifulSoup(r, "html.parser")
-
-		#find the JSON section in the HTML
-		json_tag = soup.find("script", type="application/ld+json") #that last part could be a variable I guess
-		jString = json_tag.string
-		d = json.loads(jString) #change 'd' to something more descriptive
-
-		#print(d['name']+"\n")
-		#print(d['cookTime']+"\n") #this spits out a time code. maybe ignore it anyway?
-
-		#loop through the ingredients and print them to the screen. obviously we'll want them somewhere else
-		i=0
-		while i<len(d["recipeIngredient"]):
-			#print(d["recipeIngredient"][i])
-			i+=1 
-
-		print("\n")
-
-		i=0
-		while i<len(d["recipeInstructions"]):
-			#print(d["recipeInstructions"][i])
-			i+=1
-		# can probably stand to clean this portion up a bit...also printing the non-looped guys out as individual cells
-		writer.writerow([idCounter,d['name'],"A delicious recipe!",d['recipeCategory'],"",d['recipeYield'], "",""])
-		writer.writerow([d['recipeYield']])
-		try:
-			writer.writerow([d['cookTime'],d['totalTime']])
-			#writer.writerow([d['totalTime']])
-		except KeyError:
-			print("No cook time given")
-			
-		for item in d["recipeInstructions"]:
-			writer.writerow([item])
-			#writer.writerows(d["recipeInstructions"])
-		for item in d["recipeIngredient"]:
-			writer.writerow([item])
-		writer.writerow("\n")
+def build_ingredient_table(idCounter,writer):
+	writer.writerow(['Ingredient_ID', 'Ingredient_Name', 'Ingredient_Best_Store'])
+	#we want all ingredients here all together
+	allIngredients = []
+	for recipe in recipeData:
+		i = 0
+		print("breakpoint 1")
+		while i<len(recipe['recipeIngredient']):
+			if(recipe['recipeIngredient'][i]) not in allIngredients:
+				allIngredients.append(recipe['recipeIngredient'][i])
+			i += 1
+	for ingredient in allIngredients:
+		writer.writerow([idCounter+1, ingredient, ""])
 		idCounter += 1
 	
+def build_recipe_table(idCounter,writer):
+	writer.writerow(['Recipe_ID', 'Recipe_Name', 'Recipe_Description', 'Recipe_Style', 'Recipe_Skill_Level', 'Recipe_Serves', 'Recipe_Course', 'Recipe_IsHealthy'])
+	for recipe in recipeData:
+		idCounter += 1
+		writer.writerow([idCounter,recipe['name'],"A delicious recipe!",recipe['recipeCategory'],"",recipe['recipeYield'], "",""])
+	
+def build_step_ingredient_table(idCounter,writer):
+	writer.writerow(['Recipe_ID', 'Step_No', 'Ingredient_ID', 'Step_Amount', 'Unit_ID'])
+	for recipe in recipeData:
+		jData = recipe
+		idCounter += 1
+		
+	
+def build_step_table(idCounter,writer):
+	writer.writerow(['Recipe_ID', 'Step_No', 'Step_Instructions', 'Step_Time', 'Unit_ID'])
+	for recipe in recipeData:
+		idCounter += 1
+		stepNo = 0
+		while stepNo < len(recipe['recipeInstructions']):
+			writer.writerow([idCounter,stepNo+1,recipe['recipeInstructions'][stepNo],"",1])
+			stepNo += 1
+	
+def build_unit_table(x,y):
+	pass
 
-
-#print(d["recipeIngredient"]) #this will print out the ingredients as A LIST. Can do by reference which means can iterate through
-#print(len(d["recipeIngredient"]))
-
-#so can iterate through the list
-
-#name AND headline (think they're the same. this is recipe name
-#cookTime (one value)
-#totalTime (one value)
-#recipeCategory (multiple, don't know that i need this)
-#recipeIngredient (this is an important one, multiple)
-#nutrition (this looks like a hash...might be nice but "nice to have")
-#recipeInstructions (same as ingredient)
-#recipeYield (one value)
-
-"""The ideal solution here would be to generate a CSV file for each table"""
-#Recipe Step Ingredients: Recipe_ID, Step_No, Ingredient_ID, Step_Amount, Unit_ID
+idCounter=0
+tables = ('recipe','step','step_ingredient','ingredient','unit')		
+recipes = ["http://www.foodnetwork.com/recipes/food-network-kitchen/eggplant-and-tofu-curry-recipe","http://www.foodnetwork.com/recipes/rachael-ray/spinach-and-artichoke-baked-whole-grain-pasta-recipe", "http://www.foodnetwork.com/recipes/easy-mushroom-vegetable-paella-recipe", "http://www.foodnetwork.com/recipes/rachael-ray/ravioli-vegetable-lasagna-recipe","http://www.foodnetwork.com/recipes/rachael-ray/instant-pesto-torta-with-bread-and-vegetables-recipe"]
+units = ('cup', 'cups', 'tablespoon', 'tablespoons', 'can', 'cans', 'box', 'boxes', 'ounce', 'ounces', 'clove', 'cloves', 'piece', 'pieces', 'leaf', 'leaves', 'teaspoon', 'teaspoons')
+recipeData = []
+for recipe in recipes:
+	recipeData.append(createSoup(recipe))
+	
+createTables(recipeData)
